a787de4ae59f6fbd4a7383c4ae0e4524fc7ec079
i001962@gmail.com <i001962@gmail.com>
2022-07-29 16:45:41 +0300

    add ipfs demo snap

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+const { errors: rpcErrors } = require('eth-json-rpc-errors');
+const { IPFS } = require('./ipfs');
+
+const ipfs = new IPFS({
+  host: 'ipfs.infura.io',
+  protocol: 'https',
+});
+
 module.exports.onRpcRequest = async ({ origin, request }) => {
   console.log('starting....')
   let state = await wallet.request({
@@ -15,6 +23,11 @@ module.exports.onRpcRequest = async ({ origin, request }) => {
   }
 
   switch (request.method) {
+      case 'add':
+        return await ipfs.add(request.params[0]);
+      case 'cat':
+        return await ipfs.cat(request.params[0]);
+
       case 'storeAddress': 
         state.book.push({
           name:request.nameToStore,
